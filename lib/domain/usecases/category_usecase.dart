199ff1646cc1a2d49b4c2c1b7236cf050a74347c
Edmir Soares <edmir@neocity.com.br>
2026-02-13 11:25:01 -0300

    feat: enhance category management with update and delete use cases in cubit

--- lib/domain/usecases/category_usecase.dart
+++ lib/domain/usecases/category_usecase.dart
@@ -1,24 +1,53 @@
-// lib/domain/usecases/category_usecase.dart
 import 'package:gerenciamento_agricola/domain/entities/category_entity.dart';
 import 'package:gerenciamento_agricola/domain/repositories/category_repository.dart';
 
 class CreateCategoryUseCase {
   final ICategoryRepository _repository;
-  
+
   CreateCategoryUseCase(this._repository);
-  
+
   Future<void> call(String name) async {
     final category = CategoryEntity.create(name: name);
     await _repository.saveCategory(category);
   }
 }
 
+class UpdateCategoryUseCase {
+  final ICategoryRepository _repository;
+
+  UpdateCategoryUseCase(this._repository);
+
+  Future<void> call(CategoryEntity category) async {
+    await _repository.updateCategory(category);
+  }
+}
+
 class GetAllCategoriesUseCase {
   final ICategoryRepository _repository;
-  
+
   GetAllCategoriesUseCase(this._repository);
-  
+
   Future<List<CategoryEntity>> call() async {
     return await _repository.getAllCategories();
+  }
+}
+
+class GetCategoryByIdUseCase {
+  final ICategoryRepository _repository;
+
+  GetCategoryByIdUseCase(this._repository);
+
+  Future<CategoryEntity?> call(int id) async {
+    return await _repository.getCategoryById(id);
+  }
+}
+
+class DeleteCategoryByIdUseCase {
+  final ICategoryRepository _repository;
+
+  DeleteCategoryByIdUseCase(this._repository);
+
+  Future<void> call(int id) async {
+    await _repository.deleteCategory(id);
   }
 }
--- lib/presentation/cubits/category_cubit.dart
+++ lib/presentation/cubits/category_cubit.dart
@@ -4,26 +4,63 @@ import 'category_state.dart';
 
 class CategoryCubit extends Cubit<CategoryState> {
   final CreateCategoryUseCase _createUseCase;
+  final UpdateCategoryUseCase _updateUseCase;
+  final DeleteCategoryByIdUseCase _deleteUseCase;
   final GetAllCategoriesUseCase _getAllUseCase;
-  
-  CategoryCubit(this._createUseCase, this._getAllUseCase) 
-    : super(CategoryInitial());
-  
+  final GetCategoryByIdUseCase _getByIdUseCase;
+
+  CategoryCubit(
+    this._createUseCase,
+    this._getAllUseCase,
+    this._getByIdUseCase,
+    this._updateUseCase,
+    this._deleteUseCase,
+  ) : super(CategoryInitial());
+
   Future<void> createCategory(String name) async {
     emit(CategoryLoading());
     try {
       await _createUseCase(name);
-      emit(CategorySuccess());
+      await loadCategories();
     } catch (e) {
       emit(CategoryError(e.toString()));
     }
   }
-  
+
+  Future<void> updateCategory(int id, String name) async {
+    emit(CategoryLoading());
+    try {
+      final category = await _getByIdUseCase(id);
+      if (category == null) {
+        emit(CategoryError("Categoria não encontrada"));
+        return;
+      }
+      final updatedCategory = category.copyWith(
+        name: name,
+        updatedAt: DateTime.now(),
+      );
+      await _updateUseCase(updatedCategory);
+      await loadCategories();
+    } catch (e) {
+      emit(CategoryError(e.toString()));
+    }
+  }
+
   Future<void> loadCategories() async {
     emit(CategoryLoading());
     try {
       final categories = await _getAllUseCase();
       emit(CategoriesLoaded(categories));
+    } catch (e) {
+      emit(CategoryError(e.toString()));
+    }
+  }
+
+  Future<void> deleteCategory(int id) async {
+    emit(CategoryLoading());
+    try {
+      await _deleteUseCase(id);
+      await loadCategories();
     } catch (e) {
       emit(CategoryError(e.toString()));
     }
